faf98949dabd3db293ca21ce81ac556daaee967e
xuyunfei <905231404@qq.com>
2019-09-16 14:55:32 +0800

    完成了订单控制器的编写。

---
+++ src/main/java/com/bdqn/controller/BillController.java
@@ -0,0 +1,16 @@
+package com.bdqn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ClassName: {@link BillController}
+ * Description: TODO 订单控制器
+ * Author: xyf
+ * Date 2019/9/16 14:52
+ */
+@Controller
+@RequestMapping("/bill")
+public class BillController {
+
+}
